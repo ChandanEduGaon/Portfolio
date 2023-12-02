404e0437e88a9ab573a7342091a316cfc414a5a9
Chandan Kumar <singh963130@gmail.com>
2023-12-02 20:19:22 +0530

    UI Upgrade

--- src/components/common/Card.jsx
+++ src/components/common/Card.jsx
@@ -3,10 +3,7 @@ import "boxicons";
 
 const Card = (props) => {
   return (
-    <div
-      className="w-[90%] h-[15%] rounded-full p-5 flex justify-start items-center mx-5 my-2 md:w-[35%] md:h-[15%] sm:w-[45%] sm:h-[15%] lg:w-[25%] lg:h-[15%] hover:cursor-pointer card shadow"
-      style={{ backgroundColor: props.bgColor }}
-    >
+    <div className="w-[35%] h-[25%] rounded p-5 flex justify-start items-center mx-5 my-2 md:w-[35%] md:h-[15%] sm:w-[40%] sm:h-[25%] lg:w-[25%] lg:h-[15%] hover:cursor-pointer card border border-dashed flex-col md:flex-row">
       <span className="mx-2">
         <box-icon
           type={props.iconType ? props.iconType : "logo"}
@@ -16,10 +13,10 @@ const Card = (props) => {
         ></box-icon>
       </span>
       <div className="details">
-        <h1 className="text-md md:text-xl text-white font-bold">
+        <h1 className="text-md md:text-xl text-gray-500 font-bold">
           {props.title}
         </h1>
-        <p className="text-white text-sm">{props.sub_title}</p>
+        <p className="text-gray-400 text-sm hidden md:block md:text-[12px] lg:text-[15px]">{props.sub_title}</p>
       </div>
     </div>
   );
--- src/components/common/Service.jsx
+++ src/components/common/Service.jsx
@@ -3,9 +3,9 @@ import React from "react";
 const Service = (props) => {
   return (
     <div
-      className={`sm:w-[80%] sm:h-[100%] w-[95%] flex justify-between items-center p-5 md:p-10 border bottom-[.1px] rounded-lg ${
+      className={`sm:w-[80%] sm:h-[100%] w-[90%] flex justify-between items-center p-5 md:p-10 border bottom-[.1px] rounded-lg ${
         props.align === "0" ? "flex-row-reverse" : ""
-      } mb-10 flex-col sm:flex-row`}
+      } mb-10 flex-col sm:flex-row vintage border border-dashed border-white`}
     >
       <div className="w-[100%] flex flex-col items-start p-3">
         <span className="font-bold text-4xl mb-1">{props.title}</span>
@@ -22,7 +22,7 @@ const Service = (props) => {
         <img
           src={props.image}
           alt="logo"
-          className="transition duration-200 ease-in-out w-[100%] h-[50%] rounded max-w-[400px] max-h-[250px] min-w-[400px] min-h-[250px] hover:scale-125 overflow-hidden"
+          className="transition duration-500 ease-in-out w-[100%] h-[50%] rounded max-w-[400px] max-h-[250px] min-w-[400px] min-h-[250px] hover:scale-105 cursor-pointer overflow-hidden"
         />
       </div>
     </div>
@@ -30,3 +30,18 @@ const Service = (props) => {
 };
 
 export default Service;
+
+{
+  /* <span
+          className="w-10 h-10 rounded-full bg-slate-400 bg-opacity-25 flex justify-center items-center absolute left-2 md:left-20 cursor-pointer top-48 active:bg-purple-600 hover:bg-purple-400 border"
+          onClick={() => updateService_no(service_no + 1)}
+        >
+          <box-icon name="left-arrow-alt" color="#ffffff"></box-icon>
+        </span>
+        <span
+          className="w-10 h-10 rounded-full bg-slate-400 bg-opacity-25 flex justify-center items-center absolute right-2 md:right-20 cursor-pointer top-48 active:bg-purple-600 hover:bg-purple-400"
+          onClick={() => updateService_no(service_no + 1)}
+        >
+          <box-icon name="right-arrow-alt" color="#ffffff"></box-icon>
+        </span> */
+}
